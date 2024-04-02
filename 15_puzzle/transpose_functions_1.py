e9db9d25f8c4aafc9b8ce93fb88069365a10d091
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-04-02 17:34:38 +0900

    change file name

---
+++ 15_puzzle/transpose_functions_1.py
@@ -0,0 +1,11 @@
+def apply(lst, trans):
+    first, second = trans
+    lst[first], lst[second] = lst[second], lst[first]
+
+
+word = ['S', 'T', 'O', 'P']
+transpositions = [(0, 1), (1, 2), (2, 3)]
+
+for transposition in transpositions:
+    apply(word, transposition)
+print(word)
