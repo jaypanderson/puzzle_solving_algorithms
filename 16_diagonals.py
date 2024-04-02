86eb2e80c20141528d79c5fb5b201f610f4b37e2
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-04-02 17:51:40 +0900

    add documentation

--- 16_diagonals.py
+++ 16_diagonals.py
@@ -1,3 +1,33 @@
+"""
+algorithm that solves the 16 diagonals problem. there is a board with five rows and 5 columns with a total of 25 cells.
+The goal is to place 16 diagonal lines that go from the corner of a cell to the opposite corner.  But the edges of these
+diagonals must not touch others edges from other cells.  This algorithm gives the following solution.
+
+[[1, 1, 1, 0, 2],
+ [0, 0, 1, 0, 2],
+ [2, 2, 0, 2, 2],
+ [2, 0, 1, 0, 0],
+ [2, 0, 1, 1, 1]]
+
+The 0s represent empty cells, the 1s represent a '/' (lower left to upper right (slash)) and 2s represent a '\'
+(upper left to lower right (backslash)).  So if we convert it it looks something like this.
+
+[[/, /, /, 0, \],
+ [0, 0, /, 0, \],
+ [\, \, 0, \, \],
+ [\, 0, /, 0, 0],
+ [\, 0, /, /, /]]
+
+if we move the formating it looks more like this
+
+ / / /   \
+     /   \
+ \ \   \ \
+ \   /
+ \   / / /
+
+"""
+
 def can_extend(perm, i, j, direc, n):
     ul_lr = [[-1, -1], [1, 1]]
     ur_ll = [[-1, 1], [1, -1]]
