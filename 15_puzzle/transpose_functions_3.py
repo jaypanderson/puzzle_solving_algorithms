7ffb790da0b47bf9aecb5c4db8f0aad9c2689510
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-04-05 13:28:19 +0900

    Update transpose_functions_3.py

--- 15_puzzle/transpose_functions_3.py
+++ 15_puzzle/transpose_functions_3.py
@@ -1,7 +1,20 @@
+def convert(transposition):
+    """
+    converts a normal transposition into a series of neighbor transpositions
+    :param transposition:
+    :return:
+    """
+    i, j = transposition
+    assert i != j
+    i, j = min(i, j), max(i, j)
+
+    return [(s, s + 1) for s in range(i, j)] + [(s, s + 1) for s in reversed(range(i, j - 1))]
+
 
 def transform(first: str, second: str) -> None:
-    
+    pass
 
 
 if __name__ == '__main__':
+    print(convert((2, 5)))
     transform('MARINE', 'AIRMEN')
