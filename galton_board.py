a91f59c62a5964454cd94262aeedf13c0f15914d
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-04-25 21:44:09 +0900

    update precision

--- galton_board.py
+++ galton_board.py
@@ -3,11 +3,12 @@ simulates the galton board and you can choose the depth of the galton board. Thi
 generators so the answer will be exact every single time. it specically calculates the amount between the 40 percent and
 60 percent area
 """
-
+from decimal import Decimal, getcontext
 from itertools import product
 
-ans = [1]
-n = 50
+getcontext().prec = 1000
+ans = [Decimal(1)]
+n = 100
 
 for i in range(2, n+1):
     temp = [None] * i
@@ -23,4 +24,5 @@ for i in range(2, n+1):
 
 print(ans)
 print(len(ans))
-print(sum(ans[int(n * 0.4):int(n * 0.6)]))
+#print(sum(ans[39:60]))
+print(sum(ans[int(n * 0.3999999999):int(n * 0.6)]))
