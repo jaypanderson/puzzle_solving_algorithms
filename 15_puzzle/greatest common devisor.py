c48ab55291d78dc8a3c2514ebfd7b69171438086
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-02 13:28:51 +0900

    add new function

--- 15_puzzle/greatest common devisor.py
+++ 15_puzzle/greatest common devisor.py
@@ -1,8 +1,14 @@
 """
 algorithm that finds the greatest common devisor between two numbers(gcd)
 """
-
-def gcd(a:int, b:int) -> int:
+import time
+def gcd_brute_force(a:int, b:int) -> int:
+    """
+    the brute force method which will take too long for very large numbers
+    :param a:
+    :param b:
+    :return:
+    """
     if a < 0 or b < 0:
         print("must be positive numbers")
         return
@@ -13,6 +19,23 @@ def gcd(a:int, b:int) -> int:
         if a % num == 0 and b % num == 0:
             return num
 
+def gcd(a:int, b:int) -> int:
+    """
+    a better method that avoids a large amount of calculations.
+    :param a:
+    :param b:
+    :return:
+    """
+
+    while a > 0 and b > 0:
+        if a >= b:
+            a -= b
+        else:
+            b -= a
+    return max(a, b)
+
 
 if __name__ == '__main__':
-    print(gcd(6962, 10443))
+    start = time.time()
+    print(gcd(69620001, 1044300000))
+    print(time.time() - start)
